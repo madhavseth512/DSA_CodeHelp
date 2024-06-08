1c9ab527a6f9402becf0338ac4f42547f752e035
madhavseth512 <sethmadhav125@gmail.com>
2024-06-08 16:07:19 +0530

    code for char array reversal

---
+++ CharacterArrays_Strings/CharArrayReversal.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+//Getting the size of the entered string as we need information about the sizein string reversal
+int get_count(char words[])
+{
+    int count = 0;
+    int i = 0;
+    while (words[i] != '\0')
+    {
+        count ++;
+        i++;
+    }
+    return count;
+}
+
+int main()
+{
+    char words[100];
+    cout<<"Enter your words\n";
+    cin.getline(words,100); 
+
+    int k=0;
+    int j=get_count(words)-1;
+
+    while (k<=j)
+    {
+        //swapping the elements 
+        swap(words[k],words[j]);
+        k++;
+        j--;
+    }
+
+    //printing the reversed string
+    for(int i=0;i<get_count(words);i++)
+    {
+        cout<<words[i];
+    }
+
+    return 0;
+
+}
