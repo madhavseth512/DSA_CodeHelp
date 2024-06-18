f2faf1f7fc71f06999916ec05703730b0acad5f7
madhavseth512 <sethmadhav125@gmail.com>
2024-06-18 23:04:44 +0530

    Inclusion and Exclusion principle

---
+++ Recursion_Backtracking/rec_Level3/SubsequenceofString.cpp
@@ -0,0 +1,14 @@
+//We are given with a string. We have to print all subsequences of a string.
+//This is based on the principle of inclusion-exclusion
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter the string without any whitespace character\n";
+    cin >> name;
+    cout<<name<<endl;
+    
+}
